b55ab76a76bdafee9958334e7a9e7e9feb4f522c
epord <pedro.balaguer@hotmail.com>
2018-09-15 22:28:53 -0300

    Fixed Bemans method

--- src/main/java/OsciladorAmortiguado/StepCalculators/BeemanCalculator.java
+++ src/main/java/OsciladorAmortiguado/StepCalculators/BeemanCalculator.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 
 public class BeemanCalculator extends StepCalculator {
 
-    List<Particle> previousState;
+    private List<Particle> previousState;
 
     public BeemanCalculator(ForceCalculator forceCalculator, Double deltaT, List<Particle> particles) {
         super(forceCalculator, deltaT);
@@ -18,9 +18,9 @@ public class BeemanCalculator extends StepCalculator {
         previousState = particles.stream().map(p -> {
             Vector a = forceCalculator.calculateAcceleration(p, particles);
             Vector prevPos = p.getPosition()
-                    .plus(p.getVelocity().dot(-deltaT / 2.0))
-                    .plus(a.dot(deltaT * deltaT / 2));
-            Vector prevVel = p.getVelocity().minus(a.dot(deltaT));
+                    .plus(p.getVelocity().dot(-deltaT))
+                    .plus(a.dot(deltaT * deltaT / 2.0));
+            Vector prevVel = p.getVelocity().plus(a.dot(-deltaT));
 
             Particle tempParticle = p.getCopyWithVelocity(prevVel).getCopyWithPosition(prevPos);
             Vector prevAcc = forceCalculator.calculateAcceleration(tempParticle, particles);
@@ -37,7 +37,7 @@ public class BeemanCalculator extends StepCalculator {
             Vector nextPos = p.getPosition()
                     .plus(p.getVelocity().dot(deltaT))
                     .plus(currAcc.dot(deltaT * deltaT * (2.0 / 3.0)))
-                    .minus(previousParticleState.getVelocity().dot(1.0 / 6.0 * deltaT * deltaT));
+                    .minus(previousParticleState.getAcceleration().dot(1.0 / 6.0 * deltaT * deltaT));
             Vector predictedVel = p.getVelocity()
                     .plus(currAcc.dot(3.0 / 2.0 * deltaT))
                     .minus(previousParticleState.getAcceleration().dot(1.0 / 2.0 * deltaT));
@@ -45,12 +45,12 @@ public class BeemanCalculator extends StepCalculator {
             return p.getCopyWithAcceleration(currAcc).getCopyWithVelocity(predictedVel).getCopyWithPosition(nextPos);
         }).collect(Collectors.toList());
 
-        List<Particle> nextParticles = predictedParticles.stream().map(p -> {
+        List<Particle> nextParticles = particles.stream().map(p -> {
             Particle previousParticleState = previousState.get(previousState.indexOf(p));
             Particle predictedParticleState = predictedParticles.get(predictedParticles.indexOf(p));
 
             Vector prevAcc = previousParticleState.getAcceleration();
-            Vector currAcc = p.getAcceleration();
+            Vector currAcc = predictedParticleState.getAcceleration();
             Vector predictedAcc = forceCalculator.calculateAcceleration(predictedParticleState, predictedParticles); // Acceleration using predicted velocity
 
             Vector correctedVelocity = p.getVelocity()
@@ -58,7 +58,7 @@ public class BeemanCalculator extends StepCalculator {
                     .plus(currAcc.dot(5.0 / 6.0 * deltaT))
                     .minus(prevAcc.dot(1.0 / 6.0 * deltaT));
 
-            return p.getCopyWithVelocity(correctedVelocity).getCopyWithAcceleration(predictedAcc);
+            return predictedParticleState.getCopyWithVelocity(correctedVelocity).getCopyWithAcceleration(predictedAcc);
         }).collect(Collectors.toList());
 
         previousState = particles;
